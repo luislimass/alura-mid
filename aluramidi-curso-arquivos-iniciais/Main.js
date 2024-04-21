cc888c74640651fd94a5b09d7d719e02c1db6e64
Luis Eduardo <eu.luiseduardo.pro@gmail.com>
2024-04-21 15:57:12 -0300

    contador

--- aluramidi-curso-arquivos-iniciais/Main.js
+++ aluramidi-curso-arquivos-iniciais/Main.js
@@ -1,6 +1,16 @@
 function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
-
+//constante
 const ListadeTeclas = document.querySelectorAll('.tecla');
-ListadeTeclas[0].onclick=tocaSomPom; 
+//variavel
+let contador = 0;
+
+//enquato
+while (contador<9) {
+    ListadeTeclas[0].onclick=tocaSomPom;
+
+    contador= (contador + 1);
+    //dev tools
+    console. log(contador);
+}
