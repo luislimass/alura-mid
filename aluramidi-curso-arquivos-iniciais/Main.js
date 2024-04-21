52360b40f986b6d0fe90f752fc3fb4cc064e138c
Luis Eduardo <eu.luiseduardo.pro@gmail.com>
2024-04-21 14:19:11 -0300

    querry

---
+++ aluramidi-curso-arquivos-iniciais/Main.js
@@ -0,0 +1,12 @@
+function tocasomPom ()  {
+    document.querySelector('#som_tecla_pom') . play();
+}
+
+function tocasomclap () {
+    document.querySelector('#som_tecla_clap') . play ();
+}  
+
+
+
+document.querySelector('.tecla_pom');
+
